d26f5990b0fb36cdedc2d07b13bf4a26329ea9ea
Akin <akin.ozcan@mobven.com>
2024-07-17 13:47:29 +0300

    add new tests for optional image adding feature

--- Tests/ToolTipKitTests/ToolTipProtocolTest.swift
+++ Tests/ToolTipKitTests/ToolTipProtocolTest.swift
@@ -12,16 +12,19 @@ final class ToolTipProtocolTest: XCTestCase {
     class MockToolTip: ToolTipProtocol {
         var overView: UIView
         var text: NSAttributedString
+        var toolTipImage: UIImage?
         var delegate: ToolTipGestureDelegate?
 
-        init(overView: UIView, text: String) {
+        init(overView: UIView, text: String, image: UIImage? = nil) {
             self.overView = overView
             self.text = ToolTipView.customize(text: text)
+            self.toolTipImage = image
         }
 
-        init(overView: UIView, text: NSAttributedString) {
+        init(overView: UIView, text: NSAttributedString, image: UIImage? = nil) {
             self.overView = overView
             self.text = text
+            self.toolTipImage = image
         }
 
         var presentCalled: Bool = false
@@ -31,7 +34,7 @@ final class ToolTipProtocolTest: XCTestCase {
     }
 
     func testPresent() {
-        let mockToolTip = MockToolTip(overView: UIView(), text: "mockToolTip")
+        let mockToolTip = MockToolTip(overView: UIView(), text: "mockToolTip", image: UIImage(named: "test"))
         mockToolTip.present()
         XCTAssertTrue(mockToolTip.presentCalled)
     }
